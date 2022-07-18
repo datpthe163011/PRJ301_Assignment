a3fc99ae1ff1f12e824102c0c82d5b082ed7709d
datpthe163011 <datpthe163011@fpt.edu.vn>
2022-07-19 03:17:23 +0700

    create StudnetGroup

---
+++ PRJ301_Assignment/src/java/model/StudentGroup.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pc
+ */
+public class StudentGroup {
+    private String SID;
+    private String GID;
+
+    public String getSID() {
+        return SID;
+    }
+
+    public void setSID(String SID) {
+        this.SID = SID;
+    }
+
+    public String getGID() {
+        return GID;
+    }
+
+    public void setGID(String GID) {
+        this.GID = GID;
+    }
+}
