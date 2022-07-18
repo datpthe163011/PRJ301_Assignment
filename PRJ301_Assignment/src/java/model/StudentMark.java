b9a679e14f9350d0b89317c59d50300d7e4d7d61
datpthe163011 <datpthe163011@fpt.edu.vn>
2022-07-19 03:17:09 +0700

    create studnetMark

---
+++ PRJ301_Assignment/src/java/model/StudentMark.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pc
+ */
+public class StudentMark {
+    private String SID;
+    private String MKID;
+    private String GID;
+
+    public String getSID() {
+        return SID;
+    }
+
+    public void setSID(String SID) {
+        this.SID = SID;
+    }
+
+    public String getMKID() {
+        return MKID;
+    }
+
+    public void setMKID(String MKID) {
+        this.MKID = MKID;
+    }
+
+    public String getGID() {
+        return GID;
+    }
+
+    public void setGID(String GID) {
+        this.GID = GID;
+    }
+    
+}
