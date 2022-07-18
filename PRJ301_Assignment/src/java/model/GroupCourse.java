75728eced183f6c4d2c4a3fbc8b35972906de738
datpthe163011 <datpthe163011@fpt.edu.vn>
2022-07-19 03:15:51 +0700

    insert groupcourse class

---
+++ PRJ301_Assignment/src/java/model/GroupCourse.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pc
+ */
+public class GroupCourse {
+    private String GID;
+    private String CID;
+
+    public String getGID() {
+        return GID;
+    }
+
+    public void setGID(String GID) {
+        this.GID = GID;
+    }
+
+    public String getCID() {
+        return CID;
+    }
+
+    public void setCID(String CID) {
+        this.CID = CID;
+    }
+}
